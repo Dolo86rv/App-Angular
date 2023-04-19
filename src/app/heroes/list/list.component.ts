fa90a03d10008c7da6219766496d978b464d48b6
Dolorv86 <dolorv86@gmail.com>
2023-04-19 12:38:30 +0200

    se crean los modulos Counter y Heroes

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class ListComponent {
   public heroName: string[] = ['Spiderm', 'Hulk', 'Iroman', 'She Hulk', 'Thor']
+  public deletedHero?: string
+
+  removeLastHero():void{
+    this.deletedHero = this.heroName.pop()
+
+  }
 }
